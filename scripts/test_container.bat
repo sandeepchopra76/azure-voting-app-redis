d7cd3cb29f42f5e6897dc40a2d4fbeb73d0b44dd
Sandeep Chopra <sandeepchopra76@gmail.com>
2023-07-07 21:40:15 +0530

    Added scripts and test files

---
+++ scripts/test_container.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+
+set count=0
+set started=false
+
+:loop
+set /a count+=1
+echo [%STAGE_NAME%] Starting container [Attempt: %count%]
+
+powershell -Command "$testStart = Invoke-WebRequest -Uri http://localhost:8000; exit $testStart.StatusCode"
+
+if %errorlevel% equ 0 (
+    set started=true
+) else (
+    timeout /t 1 /nobreak >nul
+)
+
+if not %started%==true (
+    if %count%==3 (
+        exit /b 1
+    ) else (
+        goto loop
+    )
+)
