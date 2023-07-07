c2353f1a6fcd235d6484cfe49ff4442ef133e02d
Sandeep Chopra <sandeepchopra76@gmail.com>
2023-07-07 23:35:05 +0530

    added error handling

--- scripts/test_container.bat
+++ scripts/test_container.bat
@@ -8,7 +8,7 @@ set started=false
 set /a count+=1
 echo [%STAGE_NAME%] Starting container [Attempt: %count%]
 
-powershell -Command "try { $testStart = Invoke-WebRequest -Uri http://localhost:8080 -UseBasicParsing; exit $testStart.StatusCode } catch { exit 1 }"
+powershell -Command "try { $testStart = Invoke-WebRequest -Uri http://localhost:8080; Write-Host 'Status: ' $testStart.StatusCode; exit $testStart.StatusCode } catch { Write-Host 'Error: ' $_.Exception.Message; exit 1 }"
 
 if %errorlevel% equ 0 (
     set started=true
@@ -23,5 +23,4 @@ if not %started%==true (
         goto loop
     )
 )
-
 
